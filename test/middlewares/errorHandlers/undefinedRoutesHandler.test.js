9cd8a5efad28ae54952ccb64db85fe2840dcc703
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-09-26 00:18:09 +0300

    Add unit test for undefinedRoutesHandler module
    - Test undefinedRoutesHandler module in errorHandlers in middlewares

---
+++ test/middlewares/errorHandlers/undefinedRoutesHandler.test.js
@@ -0,0 +1,21 @@
+const undefinedRoutesHandler = require('../../../src/middlewares/errorHandlers/undefinedRoutesHandler');
+const { AppError } = require('../../../src/utils');
+
+describe('undefinedRoutesHandler', () => {
+    // Mock Exress request and response objects
+    const req = {};
+    const res = {};
+    const next = jest.fn();
+
+    it('should call next middleware by passing AppError', () => {
+        // Call undefinedRoutesHandler middleware
+        const middleware = undefinedRoutesHandler({ AppError });
+        middleware(req, res, next);
+
+        expect(next).toHaveBeenCalledWith(expect.any(AppError));
+        const error = next.mock.calls[0][0];
+
+        expect(error.statusCode).toBe(404);
+        expect(error.message).toMatch("Can't find");
+    });
+});
