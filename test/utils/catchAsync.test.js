73d3b6a7e46940e130e05de306de950358197b5c
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-09-25 22:17:45 +0300

    Add unit test for catchAsync.js in utils

---
+++ test/utils/catchAsync.test.js
@@ -0,0 +1,39 @@
+const { catchAsync } = require('../../src/utils');
+
+describe('catchAsync', () => {
+    it('should middleware a function', () => {
+        const middleware = catchAsync(() => {});
+
+        expect(typeof middleware).toBe('function');
+    });
+
+    it('should catch and pass errors to the next middleware', async () => {
+        const error = new Error('something went wrong');
+
+        const fn = async () => {
+            throw error;
+        };
+
+        const req = {};
+        const res = {};
+        const next = jest.fn();
+
+        const middleware = catchAsync(fn);
+        await middleware(req, res, next);
+
+        expect(next).toHaveBeenCalledWith(error);
+    });
+
+    it('should not call the next middleware if no error is thrown', async () => {
+        const fn = async () => {};
+
+        const req = {};
+        const res = {};
+        const next = jest.fn();
+
+        const middleware = catchAsync(fn);
+        await middleware(req, res, next);
+
+        expect(next).not.toHaveBeenCalled();
+    });
+});
