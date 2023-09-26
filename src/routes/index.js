60780e5184efc4ad920de4a7677749aab2cbc879
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-09-26 20:39:54 +0300

    Enhance routes/index.js
    - Enhance Identation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-    // Default route
-    app.use('/api', (req, res, next) => {
-        res.status(200).json({
-            success: true,
-            message: 'Welcome to the API 👋',
-        });
+  // Default route
+  app.use('/api', (req, res, next) => {
+    res.status(200).json({
+      success: true,
+      message: 'Welcome to the API 👋',
     });
+  });
 
-    // app.use('/api/v1', require('./v1'));
+  // app.use('/api/v1', require('./v1'));
 };
