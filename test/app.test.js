0b58e183b3182eb1a18e03f3ccff283099438120
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-09-26 19:41:58 +0300

    Enhance app.test.js
    - Fix Identation

--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,17 @@
 const createApp = require('../src/app');
 
 describe('createApp', () => {
-    const database = {
-        connect: jest.fn().mockResolvedValue(),
-        disconnect: jest.fn().mockResolvedValue(),
-        databaseName: 'test',
-    };
+  const database = {
+    connect: jest.fn().mockResolvedValue(),
+    disconnect: jest.fn().mockResolvedValue(),
+    databaseName: 'test',
+  };
 
-    it('should return an Express application instance', async () => {
-        // Call the createApp function with the mocked database object
-        const app = await createApp(database);
+  it('should return an Express application instance', async () => {
+    // Call the createApp function with the mocked database object
+    const app = await createApp(database);
 
-        // Assert that returned app is an instance of Express
-        expect(app).toHaveProperty('listen');
-    });
+    // Assert that returned app is an instance of Express
+    expect(app).toHaveProperty('listen');
+  });
 });
