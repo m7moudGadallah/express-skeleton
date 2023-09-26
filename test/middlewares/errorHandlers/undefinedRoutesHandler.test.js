5b3cdb89a612d8e37d696e7ea1ca46ce455726f3
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-09-26 16:15:09 +0300

    Enahnce `undefinedRoutesHandeler.test.js`
    - Enahnce identation

--- test/middlewares/errorHandlers/undefinedRoutesHandler.test.js
+++ test/middlewares/errorHandlers/undefinedRoutesHandler.test.js
@@ -2,20 +2,20 @@ const undefinedRoutesHandler = require('../../../src/middlewares/errorHandlers/u
 const { AppError } = require('../../../src/utils');
 
 describe('undefinedRoutesHandler', () => {
-    // Mock Exress request and response objects
-    const req = {};
-    const res = {};
-    const next = jest.fn();
+  // Mock Exress request and response objects
+  const req = {};
+  const res = {};
+  const next = jest.fn();
 
-    it('should call next middleware by passing AppError', () => {
-        // Call undefinedRoutesHandler middleware
-        const middleware = undefinedRoutesHandler({ AppError });
-        middleware(req, res, next);
+  it('should call next middleware by passing AppError', () => {
+    // Call undefinedRoutesHandler middleware
+    const middleware = undefinedRoutesHandler({ AppError });
+    middleware(req, res, next);
 
-        expect(next).toHaveBeenCalledWith(expect.any(AppError));
-        const error = next.mock.calls[0][0];
+    expect(next).toHaveBeenCalledWith(expect.any(AppError));
+    const error = next.mock.calls[0][0];
 
-        expect(error.statusCode).toBe(404);
-        expect(error.message).toMatch("Can't find");
-    });
+    expect(error.statusCode).toBe(404);
+    expect(error.message).toMatch("Can't find");
+  });
 });
