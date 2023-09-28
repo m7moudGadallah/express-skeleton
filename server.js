b8f45c8afd31b1574be47a6a8defb715dab5dbf0
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-09-28 10:15:08 +0300

    Create the server using http built-in module
    - In  server.js and app.js   i use async/await instead of .then for compatibility

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const http = require('http');
 require('colors');
 const database = require('./src/config');
 const createApp = require('./src/app');
@@ -10,21 +11,25 @@ const createApp = require('./src/app');
  * @throws {Error} If there's an issue starting the application.
  */
 
-const startApp = (database, createApp) => {
+const startApp = async (database, createApp) => {
   const { NODE_ENV: MODE, PORT = 3000 } = process.env;
-
-  createApp(database).then((app) => {
-    app.listen(PORT, () => {
-      console.log(
-        'App is running in '.brightMagenta.underline.bold.italic +
-          MODE.brightYellow.underline.bold.italic +
-          ' mode on port '.brightMagenta.underline.bold.italic +
-          PORT.brightYellow.underline.bold.italic +
-          ' 🚀...'.brightMagenta.underline.bold.italic
-      );
-    });
+  /*
+  -> first connect to the DB
+  -> second create server using http built-in module and pass express app 
+     to handle all the commit requests.
+  */
+  const app = await createApp(database);
+  const server = http.createServer(app).listen(PORT, () => {
+    console.log(
+      'App is running in '.brightMagenta.underline.bold.italic +
+        MODE.brightYellow.underline.bold.italic +
+        ' mode on port '.brightMagenta.underline.bold.italic +
+        PORT.brightYellow.underline.bold.italic +
+        ' 🚀...'.brightMagenta.underline.bold.italic
+    );
+    return server;
   });
 };
 
 // Start the application
-startApp(database, createApp);
+module.exports = startApp(database, createApp);
